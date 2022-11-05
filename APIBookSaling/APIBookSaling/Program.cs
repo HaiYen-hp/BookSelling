32fb791dc7c100349c5ec08e64a292a065d5cc7e
thanhvt2351 <vuongtrungthanh23052001@gmail.com>
2022-11-05 17:35:25 +0700

    Update Program.cs

--- APIBookSaling/APIBookSaling/Program.cs
+++ APIBookSaling/APIBookSaling/Program.cs
@@ -1,3 +1,10 @@
+using APIBookSaling.DbContexts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 namespace APIBookSaling
 {
     public class Program
@@ -7,19 +14,75 @@ namespace APIBookSaling
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //builder.Services.AddDbContext<ApplicationDbContext>(options => {
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            //});
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidateAudience = true,
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"])),
+                    ClockSkew = TimeSpan.Zero // remove delay of token when expire,
+                };
+                options.RequireHttpsMetadata = false;
+            });
+
+            // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Web API"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
 
             // Add Dependency Injection
+            //builder.Services.AddScoped<IUserService, UserService>();
+
             builder.Services.AddCors(p => p.AddPolicy("MyCors", build =>
             {
                 build.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
             }));
 
-
             var app = builder.Build();
-
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
