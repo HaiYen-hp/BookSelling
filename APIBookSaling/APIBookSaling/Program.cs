938739dce66d95f96728338e4dd42499005b2697
thanhvt2351 <vuongtrungthanh23052001@gmail.com>
2022-11-05 16:53:07 +0700

    .gitignore is now working

---
+++ APIBookSaling/APIBookSaling/Program.cs
@@ -0,0 +1,42 @@
+namespace APIBookSaling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Add Dependency Injection
+            builder.Services.AddCors(p => p.AddPolicy("MyCors", build =>
+            {
+                build.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+            }));
+
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseRouting();
+            app.UseCors("MyCors");
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
