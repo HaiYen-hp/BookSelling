85ca1299b0bca96c966619d194b61b988af88e12
duongtu <tudm@STE>
2022-11-08 14:44:04 +0700

    update

--- APIBookSaling/APIBookSaling/DbContexts/ApplicationDbContext.cs
+++ APIBookSaling/APIBookSaling/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using APIBookSaling.Entities;
+﻿using APIBookSaling.Constants;
+using APIBookSaling.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace APIBookSaling.DbContexts
@@ -7,5 +8,76 @@ namespace APIBookSaling.DbContexts
     {
         public DbSet<Customer> customers { get; set; }
         public DbSet<User> users { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+                entity.HasKey(e => e.Id);
+                entity.Property(s => s.Id)
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity.Property(e => e.Fullname)
+                    .HasColumnType("nvarchar(100)")
+                    .IsRequired();
+
+                entity.Property(e => e.DateOfBirth)
+                    .HasColumnType("date")
+                    .IsRequired();
+
+                entity.Property(e => e.CCCD)
+                    .HasColumnType("nvarchar(100)")
+                    .IsRequired();
+
+                entity.Property(e => e.Address)
+                    .HasColumnType("nvarchar(100)")
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("ManageUser");
+                entity.HasKey(e => e.Id);
+                entity.Property(s => s.Id)
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity.Property(e => e.UserName)
+                    .IsUnicode(false)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(e => e.Password)
+                    .IsUnicode(false)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                entity.Property(e => e.Email)
+                    .HasColumnType("nvarchar(100)")
+                    .IsRequired();
+
+                entity.Property(e => e.Phone)
+                    .HasColumnType("nvarchar(100)")
+                    .IsRequired();
+
+                entity.Property(e => e.UserType)
+                    .HasDefaultValue(UserTypes.Customer)
+                    .IsRequired();
+
+                entity.Property(e => e.CustomerId)
+                    .IsRequired();
+            });
+        }
     }
 }
